64dbfa7bd90b90ac8489d7d1d97411536b9fc3a4
Thony <thon0101@stud.kea.dk>
2021-04-15 11:08:14 +0100

    Update attributes and constructor.

--- src/Orders/Order.java
+++ src/Orders/Order.java
@@ -1,4 +1,22 @@
 package Orders;
 
+import java.util.ArrayList;
+
 public class Order {
+
+    //Attributes
+    private String nameOfCustomer;
+    private ArrayList<OrderLineItem> orderLineItems = new ArrayList<>();
+    int totalPizzas;
+    int deliveryTime;
+
+    //Constructor
+    public Order(String nameOfCustomer, ArrayList orderLineItems, int totalPizzas, int deliveryTime) {
+        this.nameOfCustomer = nameOfCustomer;
+        this.orderLineItems = orderLineItems;
+        this.totalPizzas = totalPizzas;
+        this.deliveryTime = deliveryTime;
+
+    }
+
 }
--- src/Orders/OrderLineItem.java
+++ src/Orders/OrderLineItem.java
@@ -1,4 +1,17 @@
 package Orders;
 
 public class OrderLineItem {
+
+    //Attributes
+    private int subTotal;
+    private int quantity;
+    private String comment;
+
+    //Constructor
+    public OrderLineItem(int subTotal, int quantity, String comment) {
+        this.subTotal = subTotal;
+        this.quantity = quantity;
+        this.comment = comment;
+
+    }
 }
