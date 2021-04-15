85c179200f48fec3d8e2b88ffd29a8d4e13fd93a
victorvibe <victorvibe1998@gmail.com>
2021-04-15 09:58:33 +0200

    Menu class update

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -1,4 +1,20 @@
 package Menu;
+import java.util.Scanner;
 
 public class Menu {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void mainMenu() {
+        System.out.println("Welcome");
+        System.out.println("Press 1 to create a order");
+        System.out.println("Press 2 to check orderlist");
+        System.out.println("Press 3 to login");
+    }
+
+    public int getUserInput() {
+        int userInput = scanner.nextInt();
+        return userInput;
+    }
+
 }
