0585e6f397d7e6b3cebf354e505a6cd26af84930
victorvibe <victorvibe1998@gmail.com>
2021-04-15 10:04:05 +0200

    Main menu class update

--- src/Menu/MainMenu.java
+++ src/Menu/MainMenu.java
@@ -5,5 +5,16 @@ public class MainMenu {
     public static void main(String[] args) {
 
         Menu menu = new Menu();
+
+        boolean userWantToExit = false;
+        while (userWantToExit == false) {
+            int userInput = menu.getUserInput();
+            switch (userInput) {
+                case 1:
+                    System.out.println("You chose to create a new order");
+
+
+            }
+        }
     }
 }
