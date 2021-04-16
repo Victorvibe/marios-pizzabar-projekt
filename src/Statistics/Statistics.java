cf0102bc4c87380948126ce9fe06fa0dfa41f6cb
Sonja <sonja_nawaz@hotmail.com>
2021-04-16 16:09:55 +0200

    Statistics foreach loop i gang.

--- src/Statistics/Statistics.java
+++ src/Statistics/Statistics.java
@@ -1,5 +1,25 @@
 package Statistics;
 
+import Orders.Order;
+import Pizzas.Pizza;
+
+import java.util.ArrayList;
+
 public class Statistics {
+    public static ArrayList<Order> allOrders;
+
+    public void amountSoldOfEachPizza() {
+        //for (typepåliste variabelnavn : listenavn)
+        for (Order o : allOrders) {
+            ArrayList<Pizza> a = o.getPizzaLineItemList();
+        }
+        //counter, find ud af de pizza objekter på listen, lav en counter og tæl op.
+        for (Order o:
+             ) {
+
+        }
+    }
+
+
 
 }
