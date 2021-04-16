df917636e8e51a1d601d5664daefd9c12c455647
Sonja <sonja_nawaz@hotmail.com>
2021-04-16 12:07:27 +0200

    Controller case 2

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,6 +1,12 @@
 package Controller;
 
+import Menu.Menu;
+import Orders.OrderList;
+
 public class Controller {
-
-
+   //Case 2 i Menu
+    OrderList orderList = new OrderList();
+    public void checkOrderList () {
+        orderList.printListOfOrders();
+    }
 }
