8ce7322dfd7a728d6ed55f1503b0fcb2743e1196
Sonja <sonja_nawaz@hotmail.com>
2021-04-16 13:35:16 +0200

    Controller case 1

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -2,11 +2,20 @@ package Controller;
 
 import Menu.Menu;
 import Orders.OrderList;
+import Pizzas.Pizza;
+
+import java.util.ArrayList;
 
 public class Controller {
    //Case 2 i Menu
     OrderList orderList = new OrderList();
     public void checkOrderList () {
         orderList.printListOfOrders();
+    }
+
+
+    //Case 1 i Menu
+    public void createOrder (String nameOfCustomer, ArrayList<Pizza> pizzaLineItemList, int totalPizzas, int pickupTime, int totalPrice) {
+
     }
 }
--- src/Statistics/Statistics.java
+++ src/Statistics/Statistics.java
@@ -1,4 +1,5 @@
 package Statistics;
 
 public class Statistics {
+
 }
