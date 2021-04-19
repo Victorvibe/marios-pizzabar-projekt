9aec87eb4701f750bb8ce0911c43a81f7f069093
victorvibe <victorvibe1998@gmail.com>
2021-04-19 13:18:35 +0200

    better menu with more cases

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -20,11 +20,13 @@ public class Menu {
 
     public void mainMenu() {
         System.out.println();
-        System.out.println("Welcome!");
-        System.out.println("Press 1 to create a order");
-        System.out.println("Press 2 to check the orderlist");
-        System.out.println("Press 3 to login");
-        System.out.println("Press 4 to exit program");
+        System.out.println("----- Welcome! -----");
+        System.out.println(">> Press 1 to create a order <<");
+        System.out.println(">> Press 2 to check the orderlist <<");
+        System.out.println(">> Press 3 to login <<");
+        System.out.println(">> Press 4 to see the pizzalist <<");
+        System.out.println(">> Press 5 to complete order <<");
+        System.out.println(">> Press 6 to exit <<");
 
 
             switch (getUserInput()) {
@@ -60,13 +62,24 @@ public class Menu {
                     break;
 
                 case 4:
+                    //controller.printPizzaList;
                     break;
+
+                case 5:
+                    //Controller skal kalde på metode hvor man kan complete en order
+                    break;
+
+                case 6:
+                    break;
+
+
             }
         }
 
         public void newOrder() {
         Scanner userInput = new Scanner(System.in);
 
+            //controller.printPizzaList;
             System.out.println("Enter the customers name: ");
             String name = userInput.nextLine();
             Order newOrder = controller.createNewOrder(name);
