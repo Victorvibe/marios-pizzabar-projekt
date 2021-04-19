d4974010dc0099c56c210d6f271f1e09fd94559f
Thony <thon0101@stud.kea.dk>
2021-04-19 11:03:11 +0100

    tiny changes, still not working

--- src/Statistics/Statistics.java
+++ src/Statistics/Statistics.java
@@ -1,26 +1,42 @@
 package Statistics;
 
 import Orders.Order;
+import Orders.OrderList;
 import Pizzas.Pizza;
 
 import java.util.ArrayList;
 
 public class Statistics {
-    public static ArrayList<Order> allOrders;
+    public static void main(String[] args) {
+        System.out.println("Pizza" + );
+    }
+
+    OrderList ol = new OrderList();
+
+    public ArrayList<Order> allOrders = ol.getListOfOrders();
 
     public void amountSoldOfEachPizza() {
 
         //for (typepåliste variabelnavn : listenavn)
+        ArrayList<Pizza> a = new ArrayList<>();
+        //For hver ordre tilføjer den alle dens ordres pizzaer.
         for (Order o : allOrders) {
-            ArrayList<Pizza> a = o.getPizzasInThisOrder();
+            a.addAll(o.getPizzasInThisOrder());
         }
 
-        int countPizzas = 0;
+        int skinkePizza = 0;
+        int pepperoniPizza = 0;
         //counter, find ud af de pizza objekter på listen, lav en counter og tæl op.
-        for (Order o:
-             ) {
+        for (Pizza p : a) {
+            if (p.getID() == 1) {
+            pepperoniPizza++;
+            }
+            else if (p.getID() == 2)
+                skinkePizza++;
+
 
         }
+        System.out.println("Pepperoni Pizzaer: " + pepperoniPizza + "\nSkinke pizzaer: " + );
 
     }
 
